95c09e09a15d9a2ce7afd0684245edd349103f33
akias <saika@toki.waseda.jp>
2018-10-22 16:19:48 +0900

    feat: 制作一个标签

--- src/index.js
+++ src/index.js
@@ -3,24 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Title from './title';
 
 class Header extends Component {
-  renderGoodWord (goodWord, badWord) {
-    const isGoodWord = true
-    return isGoodWord ? goodWord : badWord
-  }
-
   render () {
     const word = 'new app'
     return (
       <div>
-        <h1>
-          React {word}
-          {this.renderGoodWord(
-          <strong> is good</strong>,
-          <span> is not good</span>
-        )}
-        </h1>
+        <Title />
       </div>
     )
   }
