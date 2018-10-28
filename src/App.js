614908971a9730843481c904f1de74a653ea2b5b
akias <saika@toki.waseda.jp>
2018-10-28 11:32:00 +0900

    feat: router list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
+const FRIENDS = [
+  {
+    id: 'serval',
+    nameJa: 'サーバル',
+    nameEn: 'Serval Cat',
+    family: 'ネコ目ネコ科ネコ属'
+  },
+  {
+    id: 'raccoon',
+    nameJa: 'アライグマ',
+    nameEn: 'Common raccoon',
+    family: 'ネコ目アライグマ科アライグマ属'
+  },
+  {
+    id: 'fennec',
+    nameJa: 'フェネック',
+    nameEn: 'Fennec',
+    family: 'ネコ目イヌ科キツネ属'
+  }
+]
+
+const friendById = id => FRIENDS.find(friend => friend.id === id)
 
 const App = () => (
   <BrowserRouter>
@@ -33,10 +55,48 @@ const About = () => (
     <p>フレンズに投票するページです</p>
   </div>
 )
+
 const Friends = () => (
   <div>
     <h2>Friends</h2>
-    <p>ここにフレンズのリストを書きます</p>
+    <Route exact path='/friends' component={FriendList} />
+    <Route path='/friends/:id' component={Friend} />
+    {/* <p>ここにフレンズのリストを書きます</p> */}
+  </div>
+)
+
+const Friend = props => {
+  const { id } = props.match.params
+  const friend = friendById(id)
+
+  if (typeof friend === 'undefined')  {
+    return (
+      <div>
+        <p>Friends with id '{id}' does not exist.</p>
+      </div>
+    )
+  }
+  const containerStyle = { border: '1px gray solid', display: 'inline-block', padding: 10 }
+  const contentsStyle = { margin: 0 }
+
+  return (
+    <div>
+      <div style={containerStyle}>
+        <p style={contentsStyle}>{friend.family}</p>
+        <h1 style={contentsStyle}>{friend.nameJa}</h1>
+        <p style={contentsStyle}>{friend.nameEn}</p>
+      </div>
+    </div>
+  )
+}
+
+const FriendList = () => (
+  <div>
+    {FRIENDS.map(friend => (
+      <li key={friend.id}>
+        <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
+      </li>
+    ))}
   </div>
 )
 
